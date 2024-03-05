c7103ed6519bead74572cd8946091d407aef6c78
이재민 <rkwoals524@hanmail.net>
2024-03-05 09:35:21 +0900

    initial commit

---
+++ src/main/java/com/study/performancetestapplication/PerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.performancetestapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceTestApplication.class, args);
+    }
+
+}
