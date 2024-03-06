3c070ee47018f7e2ef6fe435ee983b104b58d0bb
이재민 <rkwoals524@hanmail.net>
2024-03-06 17:38:02 +0900

    create entity

---
+++ src/main/java/com/study/performancetestapplication/controller/EhcacheController.java
@@ -0,0 +1,44 @@
+package com.study.performancetestapplication.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import org.springframework.cache.ehcache.EhCacheCache;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class EhcacheController {
+
+    private final EhCacheCacheManager cacheManager;
+
+    @GetMapping("/api/ehcache")
+    public Object findAll(){
+
+        List<Map<String, List<String>>> result = cacheManager.getCacheNames().stream()
+            .map(cacheName -> {
+                EhCacheCache cache = (EhCacheCache) cacheManager.getCache(cacheName);
+                Ehcache ehcache = cache.getNativeCache();
+                Map<String, List<String>> entry = new HashMap<>();
+
+                ehcache.getKeys().forEach(key -> {
+                    Element element = ehcache.get(key);
+                    if (element != null) {
+                        entry.computeIfAbsent(cacheName, k -> new ArrayList<>()).add(element.toString());
+                    }
+                });
+
+                return entry;
+            })
+            .collect(Collectors.toList());
+
+        return result;
+    }
+}
---
+++ src/main/java/com/study/performancetestapplication/entity/Notice.java
@@ -0,0 +1,30 @@
+package com.study.performancetestapplication.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "notices")
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String content;
+    private String who;
+    @CreationTimestamp
+    private LocalDateTime createDate;
+    @UpdateTimestamp
+    private LocalDateTime updateDate;
+}
